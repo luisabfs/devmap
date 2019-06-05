3c41f7fa5f4307d442f3407ed901b7059841a7be
Luísa Barros <qlullyq@gmail.com>
2019-06-04 21:32:44 -0300

    feat(map): add map with Mapbox and React Map GL

--- src/App.js
+++ src/App.js
@@ -1,11 +1,69 @@
-import React from "react";
+import React, { Component } from "react";
+import MapGL, { Marker } from "react-map-gl";
 
-function App() {
-  return (
-    <div className="App">
-      <h1>DevMap</h1>
-    </div>
-  );
+import "mapbox-gl/dist/mapbox-gl.css";
+
+export default class App extends Component {
+  state = {
+    viewport: {
+      width: window.innerWidth,
+      height: window.innerHeight,
+      latitude: -23.5439948,
+      longitude: -46.6065452,
+      zoom: 14
+    }
+  };
+
+  componentDidMount() {
+    window.addEventListener("resize", this._resize);
+    this._resize();
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener("resize", this._resize);
+  }
+
+  _resize = () => {
+    this.setState({
+      viewport: {
+        ...this.state.viewport,
+        width: window.innerWidth,
+        height: window.innerHeight
+      }
+    });
+  };
+
+  handleMapClick(e) {
+    const [longitude, latitude] = e.lngLat;
+
+    alert(`Latitude: ${latitude} \nLongitude: ${longitude}`);
+  }
+
+  render() {
+    return (
+      <MapGL
+        {...this.state.viewport}
+        onClick={this.handleMapClick}
+        mapStyle="mapbox://styles/mapbox/basic-v9"
+        mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_ACCESS_TOKEN}
+        onViewportChange={viewport => this.setState({ viewport })}
+      >
+        <Marker
+          latitude={-23.5439948}
+          longitude={-46.6065452}
+          onClick={this.handleMapClick}
+          captureClick={true}
+        >
+          <img
+            style={{
+              borderRadius: 100,
+              width: 48,
+              height: 48
+            }}
+            src="https://avatars2.githubusercontent.com/u/2254731?v=4"
+          />
+        </Marker>
+      </MapGL>
+    );
+  }
 }
-
-export default App;
